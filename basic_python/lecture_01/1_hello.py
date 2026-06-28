842477225427f12ccd95249155dc190f09826830
The_bangshu <debansuadhikary@gmail.com>
2026-06-28 21:29:29 +0530

    Create 1_hello.py with Python basics and examples
    Added introductory comments explaining Python basics and usage.

---
+++ basic_python/lecture_01/1_hello.py
@@ -0,0 +1,11 @@
+# Here we are trying to learn the basics of python, via CLI abv. Command Line Interface 
+
+# use code hello.py to make a new file & python hello.py to run the program
+
+# use the entire path of the code in order to make run the file or change the directory to that folder where the code is saved
+
+# The programming languages use a predetermined set of instructions that is stored under a set named 'functions' and we use those functions with the help of 'arguments' that we pass
+
+print("hello, world")
+
+# The output of this function is called as 'side effects' and the mistakes that we do are termed as 'bugs'
