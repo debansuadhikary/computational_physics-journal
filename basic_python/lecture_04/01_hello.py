50b4adbeeba6568899c557f16b727a5642bb7272
The_bangshu <debansuadhikary@gmail.com>
2026-07-02 18:19:12 +0530

    Add comments on exceptions and syntax errors
    Added comments explaining exceptions and syntax errors.

---
+++ basic_python/lecture_04/01_hello.py
@@ -0,0 +1,9 @@
+# Here we will learn about exceptions and syntax errors in our code, that we need to debug before executing the file 
+
+# print("hello, world)
+print ("hello, world")
+      
+# SyntaxError: unterminated string literal (detected at line 2)
+# They are our responsibilties to resolve 
+
+# Other kind of errors also occur, for eg., runtime error
